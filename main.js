988175e17394f2c699ed743ea8f0bc18db77eec1
hardikgithu <118292827+hardikgithu@users.noreply.github.com>
2023-02-01 20:39:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML="";
+    Recognition.start();
+}
+Recognition.onresult = function(event)
+{
+    console.log(event);
+    Content = event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById('textbox').innerHTML=Content;
+}
